2670620d9f3a7d40323b69b289da475b6dca683c
Hitansh Parikh <hitanshpparikh@gmail.com>
2024-11-08 23:36:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function windowClick(windowNumber) {
+    let url;
+    switch (windowNumber) {
+        case 1:
+            url = "https://hitanshparikh.com";  // Replace with your link for window 1
+            break;
+        case 2:
+            url = "https://github.com/hitanshparikh";  // Replace with your link for window 2
+            break;
+        case 3:
+            url = "https://hitanshparikh.github.io/old-website";  // Replace with your link for window 3
+            break;
+        default:
+            url = "#";  // Default action or URL if needed
+    }
+    window.open(url, "_blank");  // Opens link in a new tab
+}
+
